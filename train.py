f5920c50a954a5c332fb0394c71bd3cb3de0ab7b
Yu <40760155+PatrickYu1994@users.noreply.github.com>
2019-07-11 17:49:22 +1000

    Add files via upload

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ eeg_signals = ['C3', 'F7', 'F4', 'C4', 'Fz', 'Cz', 'Pz', 'Fp1',
                'P3', 'Fp2', 'P4', 'F3', 'F8']
 ecg_signals = ['ECG']
 res_signals = ['THO-', 'THO+', 'Air Flow']
+hz = 250 # hertz = 250
 
 # normalization function
 # input: array
@@ -54,21 +55,44 @@ def read_csv(xlsx_path, sheet_name = "Seizure Information"):
     temp = {}
     for _, row in df.iterrows():
         if not pd.isnull(row["Recording Start"]):
-            #print(type(pd.to_timedelta(df["Recording Start"].astype(str))))
             temp[row["Patient ID"]] = row["Recording Start"]
     for _, row in df.iterrows():
         if pd.isnull(row["Recording Start"]) and not pd.isnull(row["Patient ID"]):
             row["Recording Start"] = temp[row["Patient ID"]]
     return df
 
+# function change time to corresponding index
+# input: dataframe of time
+# output: dataframe of index
+def time_to_index(recording, seizure):
+    recording = pd.to_timedelta(recording.astype(str))
+    seizure = pd.to_timedelta(seizure.astype(str))
+    index = ((seizure - recording) / np.timedelta64(1, 's')) * hz
+    return index
+
+# function calculate corresponding seizure start / index for each patient
+# input: patient ID, dataframe from function - read_csv()
+# output: array containing seizure start / end index - [[], [], []]
 def generate_seizure_index(patient_id, df):
     df = df[df["Patient ID"] == patient_id]
+    seizure = []
+    start_index = time_to_index(df["Recording Start"], df["Seizure Start"])
+    end_index = time_to_index(df["Recording Start"], df["Seizure End"])
+    for start, end in zip(start_index, end_index):
+        if start < 0:
+            start = start + 24 * 3600 * hz
+        if end < 0:
+            end = end + 24 * 3600 * hz
+        seizure.append([start, end])
+    return seizure
 
 def xy_gen(path, xlsx_path, sheet_name = "Seizure Information"):
-    df = read_csv(path, xlsx_path, sheet_name)
+    df = read_csv(xlsx_path, sheet_name)
     for file_name in read_file_name(path):
         patient_id = file_name[-10:-4]
-        print(patient_id)
+        seizure_indexs = generate_seizure_index(patient_id, df)
+        eeg_data, ecg_data, res_data = read_data(file_name, eeg_signals, ecg_signals, res_signals)
+        print(type(eeg_data[0:1]))
 
 xy_gen(path, xlsx_path, sheet_name)
 

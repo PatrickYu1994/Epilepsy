34437fb7138c4aebce969383001aa4155fac8a73
Yu <40760155+PatrickYu1994@users.noreply.github.com>
2019-07-11 20:51:40 +1000

    Add files via upload

--- train.py
+++ train.py
@@ -14,7 +14,14 @@ eeg_signals = ['C3', 'F7', 'F4', 'C4', 'Fz', 'Cz', 'Pz', 'Fp1',
                'P3', 'Fp2', 'P4', 'F3', 'F8']
 ecg_signals = ['ECG']
 res_signals = ['THO-', 'THO+', 'Air Flow']
+
 hz = 250 # hertz = 250
+window_size = 500
+stride = 250
+p_n_rate = 1/2 # seizure:non-seizure rate = 1:2
+train_rate = 0.7 # 70% patients data are used for training model
+val_rate = 0.2 # 20% patients data are used for validation
+test_rate = 0.1 # 10% patients data are used for testing
 
 # normalization function
 # input: array
@@ -86,13 +93,38 @@ def generate_seizure_index(patient_id, df):
         seizure.append([start, end])
     return seizure
 
+def window_gen(eeg_data, ecg_data, res_data, seizure_indexs):
+    batch_x_eeg = []
+    batch_x_ecg = []
+    batch_x_res = []
+    batch_y = []
+    for index in range(0, (len(eeg_data) - window_size), stride):
+        y = 0 # where 0 stands no_seizure, 1 stands for seizure
+        for s_arr in seizure_indexs:
+            if (index > s_arr[0]) and ((index + window_size) < s_arr[1]):
+                y = 1
+        batch_y.append([y])
+        batch_x_eeg.append(eeg_data[index: (index + window_size)].values.
+                           reshape(1, window_size*len(eeg_data.columns))[0])
+        batch_x_ecg.append(ecg_data[index: (index + window_size)].values.
+                           reshape(1, window_size*len(ecg_data.columns))[0])
+        batch_x_res.append(res_data[index: (index + window_size)].values.
+                           reshape(1, window_size * len(res_data.columns))[0])
+    return batch_x_eeg, batch_x_ecg, batch_x_res, batch_y
+
 def xy_gen(path, xlsx_path, sheet_name = "Seizure Information"):
     df = read_csv(xlsx_path, sheet_name)
     for file_name in read_file_name(path):
         patient_id = file_name[-10:-4]
         seizure_indexs = generate_seizure_index(patient_id, df)
         eeg_data, ecg_data, res_data = read_data(file_name, eeg_signals, ecg_signals, res_signals)
-        print(type(eeg_data[0:1]))
+        batch_x_eeg, batch_x_ecg, batch_x_res, batch_y = \
+            window_gen(eeg_data, ecg_data, res_data, seizure_indexs)
+        print(len(batch_y))
+        break
 
 xy_gen(path, xlsx_path, sheet_name)
+#print(np.random.randn(2,3))
+
+
 

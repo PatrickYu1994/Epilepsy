3c3da4d350f4f51241b54ebf60310065e21a5006
Yu <40760155+PatrickYu1994@users.noreply.github.com>
2019-07-12 12:33:08 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from train import xy_gen
+
+path = "../../.."
+xlsx_path = "../../../Monash_University_Seizure_Detection_Database_" \
+            "September_2018_Deidentified.xlsx"
+sheet_name = "Seizure Information"
+
+training_set, validation_set, test_set = xy_gen(path, xlsx_path, sheet_name)

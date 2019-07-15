a37ffdc630cfff05b4b8b67551a058aa2eb1371e
Yu <40760155+PatrickYu1994@users.noreply.github.com>
2019-07-15 16:26:17 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import scipy.io as scio
 import tensorflow as tf
+import numpy as np
 
-training_set = scio.loadmat("./gen_dataset/training_set.mat")
-validation_set = scio.loadmat("./gen_dataset/validation_set.mat")
-test_set = scio.loadmat("./gen_dataset/test_set.mat")
+#training_set = scio.loadmat("./gen_dataset/training_set.mat")
+#validation_set = scio.loadmat("./gen_dataset/validation_set.mat")
+#test_set = scio.loadmat("./gen_dataset/test_set.mat")
 
-print(len(training_set['x_eeg']))
-print(len(validation_set['x_eeg']))
-print(len(test_set['x_eeg']))
+#dt = tf.data.Dataset.from_tensor_slices((test_set['x_ecg'], test_set['x_eeg'], test_set['x_res'], test_set['y']))
 
+#def build_model():
+
+
+a = [[1,2,3], [3,4,5]]
+print(np.array(a))

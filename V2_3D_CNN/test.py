56c057978dca95c288dcde9261459f721e602820
Yu <40760155+PatrickYu1994@users.noreply.github.com>
2019-08-02 23:34:07 +1000

    Add files via upload

--- V2_3D_CNN/test.py
+++ V2_3D_CNN/test.py
@@ -1,28 +1,20 @@
 import scipy.io as scio
-from main import xs_gen
+from train import xs_gen
 from keras.models import load_model
 from utils import auc, f1
 from sklearn import metrics
-import numpy as np
-#np.set_printoptions(threshold=np.inf)
 
-test_set = xs_gen(scio.loadmat("./gen_dataset/test_set.mat"))
+test_set_path = "./data_set/test_set.mat"
+best_model = './result/best_model/'
 
-model = load_model('./Model/20190725best_model.10-0.7270.h5', custom_objects={"auc": auc, "f1": f1})
-
-#prediction = model.predict(test_set[0])
-#p_rate = prediction[:, 1]
+test_set = xs_gen(scio.loadmat(test_set_path))
+model = load_model(best_model, custom_objects={"auc": auc, "f1": f1})
 prediction = model.predict_classes(test_set[0])
-y_true = scio.loadmat("./gen_dataset/test_set.mat")['y']
+y_true = scio.loadmat(test_set_path)['y']
 y_true = [arr[0] for arr in y_true]
-
-#fpr, tpr, thresholds = metrics.roc_curve(y_true, p_rate)
 fpr, tpr, thresholds = metrics.roc_curve(y_true, prediction)
-val_auc = metrics.auc(fpr, tpr) # 1_5, v2: auc:0.5732863286555002
-                                # 1_2, v1: auc:0.7396738206950123
-                                # 1_2, CNN3D V1: auc:0.4302690582959642 / fscore:0.1211890243902439
-                                # 1_2, CNN3D only EEG: auc: 0.5003379900856242 / fscore: 0.3485933078075578
-                                # 1_2, CNN3D All 3 signals: auc: 0.437584497521406 / fscore: 0.1766329346826127
+
+val_auc = metrics.auc(fpr, tpr)
 f_score = metrics.f1_score(y_true=y_true, y_pred=prediction)
 print("auc: ", val_auc)
 print("f_sc: ", f_score)
